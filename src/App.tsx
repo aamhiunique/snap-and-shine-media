503eb96cd0bb40cea23ede1d1010497160960736
kolambkar11 <root@VVI.local>
2023-12-15 09:28:27 +0530

    final changes done

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import "./App.css";
 import Imgcarousel from "./components/imgCarousel/Imgcarousel";
 import Navbar from "./components/navigation/Navbar";
 import Services from "./components/services/Services";
-
+import Footer from "./components/footer/Footer";
+import Test from "./components/test/Test";
 function App() {
   return (
     <>
-      <Navbar />
-      <Imgcarousel />
-      <Services />
+      <Test />
     </>
   );
 }
